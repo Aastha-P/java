27bb74276995500cdf9642e75d2ab0f182dd9793
Aastha Patel <114515584+Aspatelgithub@users.noreply.github.com>
2022-12-20 11:33:00 +0530

    Add files via upload

---
+++ Subclass.java
@@ -0,0 +1,25 @@
+class Sub
+{
+void f1()
+{
+System.out.println("<<<Hello Everyone>>>");
+}
+}
+ 
+class Subclass extends Sub
+{
+void f1()
+{
+System.out.println("I wish your Day Is Full Of Smile");
+}
+public static void main(String a[])
+{
+Sub r;
+Sub ob1=new Sub();
+Subclass ob2=new Subclass();
+r=ob1;
+r.f1();
+r=ob2;
+r.f1();
+}
+}
