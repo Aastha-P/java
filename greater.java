bd4573b8930e4dfcba7016878686047003feef16
Aastha Patel <114515584+Aspatelgithub@users.noreply.github.com>
2023-02-14 22:23:15 +0530

    Add files via upload
    A greater number among three given integers.

---
+++ greater.java
@@ -0,0 +1,26 @@
+                       //logical way
+import java.util.*;
+class greater
+{
+  public static void main( String a[])
+  {
+     int x,y,z;
+   Scanner sc=new Scanner(System.in);
+
+ System.out.println("Enter 1st number");
+   x=sc.nextInt();
+System.out.println("Enter 1st number");
+  y=sc.nextInt();
+System.out.println("Enter 1st number");
+   z=sc.nextInt();
+
+    if(x>y && x>z)
+      { System.out.println("1st number is greater."); }
+
+  else if(y>x && y>z)
+   {  System.out.println("2nd number is greater."); }
+
+else
+   { System.out.println("3rd number is greater."); }
+}
+}
