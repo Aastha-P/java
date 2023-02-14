38714cf50d724215451ab4aca40c6bb050a40a47
Aastha Patel <114515584+Aspatelgithub@users.noreply.github.com>
2023-02-14 22:19:10 +0530

    Add files via upload

---
+++ Check_even_odd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Check_even_odd
+{
+  public static void main( String a[])
+  {
+     int x;
+   Scanner sc=new Scanner(System.in);
+   x=sc.nextInt();
+    if(x%2==0)
+      { System.out.println("Number is even"); }
+  else
+   {  System.out.println("Number is odd"); }
+}
+}
