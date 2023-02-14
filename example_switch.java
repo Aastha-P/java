9b892d0e4974ac403240884a2cc963a775254563
Aastha Patel <114515584+Aspatelgithub@users.noreply.github.com>
2023-02-14 22:31:55 +0530

    Add files via upload
    It is a basic arithmetic operation code of java in which operations are declared in first class and main in second class.

---
+++ example_switch.java
@@ -0,0 +1,58 @@
+import java.util.*;
+class arithmetic_operation
+{  
+	public void addition(int x,int y) 	
+	{ 
+		System.out.println("Addition of numbers="+(x+y));
+	}
+   	void sub(int x,int y)
+   	{ 
+		System.out.println("Addition of numbers="+(x-y));
+	}
+	void multi(int x,int y)
+   	{ 
+		System.out.println("Addition of numbers="+(x*y));
+	}
+ 	void div(int x,int y)
+   	{ 
+		System.out.println("Addition of numbers="+(x%y));
+	}
+	
+}
+class example_switch
+{
+  	public static void main( String a[])
+  	{
+      		arithmetic_operation  ob=new arithmetic_operation();
+     		int x,y,choice;
+   		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 1st number");
+   		x=sc.nextInt();
+		System.out.println("Enter 1st number");
+   		y=sc.nextInt();
+		System.out.println("1. Addition");
+		System.out.println("2. Substratction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+		System.out.println("Enter choice:");
+     		choice=sc.nextInt();
+
+  		switch(choice)
+   		{
+     			case 1:
+				ob.addition(x,y);
+      			break;
+     			case 2:
+				ob.sub(x,y);
+       			break;
+      			case 3:
+				ob.multi(x,y);
+        			break;
+      			case 4:
+				ob.div(x,y); 
+        			break;
+    			default:
+				System.out.println("Invalid Choice");
+  		}
+	}
+}
